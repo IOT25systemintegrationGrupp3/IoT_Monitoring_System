44b2cb74440aefd85097fa8529f497aa3a1ebe42
TLAagge <battuguldur2017@gmail.com>
2026-09-22 17:11:42 +0200

    Delete device-service directory

--- device-service/src/main/java/com/iotmonitoring/device_service/exception/DeviceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.iotmonitoring.device_service.exception;
-
-public class DeviceNotFoundException extends RuntimeException {
-
-    public DeviceNotFoundException(String message) {
-        super(message);
-    }
-}
--- device-service/src/main/java/com/iotmonitoring/device_service/exception/DuplicateDeviceException.java
+++
@@ -1,8 +0,0 @@
-package com.iotmonitoring.device_service.exception;
-
-public class DuplicateDeviceException extends RuntimeException {
-
-    public DuplicateDeviceException(String message) {
-        super(message);
-    }
-}
