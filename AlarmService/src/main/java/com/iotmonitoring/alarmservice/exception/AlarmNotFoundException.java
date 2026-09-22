92d155290842685e1d38eb119941476b39388067
TLAagge <battuguldur2017@gmail.com>
2026-09-22 17:11:54 +0200

    Delete AlarmService directory

--- AlarmService/src/main/java/com/iotmonitoring/alarmservice/exception/AlarmNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.iotmonitoring.alarmservice.exception;
-
-public class AlarmNotFoundException extends RuntimeException {
-    public AlarmNotFoundException(String alarmId) {
-        super("Alarm with ID " + alarmId + " does not exist");
-    }
-}
